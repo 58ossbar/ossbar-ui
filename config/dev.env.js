5b275a26134dedf22663fc26c8b07c200ed33f06
hujun <hujun@creatorblue.com>
2022-08-15 09:50:48 +0800

    修改本地接口

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   NODE_ENV: '"development"',
-  domain: '"http://localhost:9082"',
-  baseUrl: '"http://localhost:9082/console/"'
+  domain: '"http://localhost:9084"',
+  baseUrl: '"http://localhost:9084/console/"'
 }
